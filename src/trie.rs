9b804a1775717aeef4249626b446ce4ca1cd7b21
Jay Butera <buterajay@gmail.com>
2019-02-08 23:46:40 -0600

    Use refcell, add() method

--- src/trie.rs
+++ src/trie.rs
@@ -1,17 +1,33 @@
-pub struct Trie<K,V> {
-    pub key:      K,
-    pub value:    Option<V>,
-    pub next:     Option< Box<Trie<K,V>> >,
-    pub children: Option< Box<Trie<K,V>> >,
+use std::collections::HashMap;
+use std::cmp::Eq;
+use std::hash::Hash;
+use std::cell::RefCell;
+
+pub struct Trie<K: Eq+Hash+Copy,V> {
+    pub value: Option<V>,
+    pub children: HashMap<K, RefCell<Trie<K,V>> >,
 }
 
-impl<K,V> Trie<K,V> {
-    pub fn new(_key: K, _value: Option<V>) -> Self {
-        Trie {
-            key:      _key,
-            value:    _value,
-            next:     None,
-            children: None,
+impl<K:Eq+Hash+Copy,V> Trie<K,V> {
+    pub fn new() -> Self {
+        Trie::<K,V> { value: None, children: HashMap::new() }
+    }
+
+    pub fn add<I: Iterator<Item=K>>(&mut self, mut key_set: I, _value: V)
+    {
+        if let Some(key) = key_set.next() {
+            if let Some(child) = self.children.get(&key) {
+                child.borrow_mut().add(key_set, _value);
+            }
+            else {
+                self.children.insert(key, RefCell::new(Trie::<K,V>::new()));
+                let child = self.children.get(&key).unwrap();
+
+                child.borrow_mut().add(key_set, _value);
+            }
+        }
+        else {
+            self.value = Some(_value);
         }
     }
 }
