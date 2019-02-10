26191fab99b5e904656bb03d967e2393625c0c39
Jay Butera <buterajay@gmail.com>
2019-02-10 12:05:11 -0600

    Seg fault of course

--- src/trie.rs
+++ src/trie.rs
@@ -5,7 +5,7 @@ use std::cell::RefCell;
 
 pub struct Trie<K: Eq+Hash+Copy,V> {
     pub value: Option<V>,
-    pub children: HashMap<K, RefCell<Trie<K,V>> >,
+    pub children: HashMap<K, *mut Box<Trie<K,V>> >,
 }
 
 impl<K:Eq+Hash+Copy,V:Copy> Trie<K,V> {
@@ -16,14 +16,18 @@ impl<K:Eq+Hash+Copy,V:Copy> Trie<K,V> {
     pub fn add<I: Iterator<Item=K>>(&mut self, mut key_set: I, _value: V)
     {
         if let Some(key) = key_set.next() {
-            if let Some(child) = self.children.get(&key) {
-                child.borrow_mut().add(key_set, _value);
+            if let Some(mut raw_child) = self.children.get(&key) {
+                unsafe {
+                    (**raw_child).add(key_set, _value);
+                }
             }
             else {
-                self.children.insert(key, RefCell::new(Trie::<K,V>::new()));
-                let child = self.children.get(&key).unwrap();
+                self.children.insert(key, (&mut Box::new(Trie::<K,V>::new())));
+                let raw_child = self.children.get(&key).unwrap();
 
-                child.borrow_mut().add(key_set, _value);
+                unsafe {
+                    (**raw_child).add(key_set, _value);
+                }
             }
         }
         else {
@@ -33,9 +37,9 @@ impl<K:Eq+Hash+Copy,V:Copy> Trie<K,V> {
 
     pub fn get<I: Iterator<Item=K>>(&self, mut key_set: I) -> Option<V> {
         match key_set.next() {
-            Some(key) => {
+            some(key) => {
                 match self.children.get(&key) {
-                    Some(child) => child.borrow().get(key_set),
+                    some(raw_child) => unsafe { (**raw_child).get(key_set) },
                     None => None, 
                 }
             },
